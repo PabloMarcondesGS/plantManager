ecf451934ce49a7fdbd55bff6c79c2f3e5d3ee28
pabloMarcondesGS <pablex.marcone@hotmail.com>
2021-04-20 16:21:15 -0300

    adicionando fontes e melhorando readme

--- README.md
+++ README.md
@@ -1,1 +1,9 @@
 # plantManager
+    -- Aplicativo mobile criado com React Native, expo e Typescript para gerenciar o irrigamento de plantas
+
+## Para rodar o Projeto
+    -- Instale o app expo no seu celular
+    -- Clone o projeto
+    -- Certifique de já ter na sua maquina o expo cli, node e react
+    -- Abra a pasta principal do projeto, rode o comando yarn/npm install
+    -- Após isso abra o terminal e rode o comando expo start e leia o qr code gerado( Forma de leitura pode mudar dependendo do aparelho)
--- src/pages/Welcome.tsx
+++ src/pages/Welcome.tsx
@@ -6,39 +6,39 @@ import {
         TouchableOpacity,
         StyleSheet,
         Dimensions,
-        Button
+        Button,
+        View
     } from 'react-native';
 import { Feather } from '@expo/vector-icons';
 
 import waterringImg from '../assets/watering.png';
 import colors from '../styles/colors';
+import fonts from '../styles/fonts';
 
 export function Welcome(){
     return(
         <SafeAreaView style={styles.container}>
+            <View style={styles.wrapper}>
+                <Text style={styles.title}>
+                    Gerencie {'\n'}
+                    suas plantas de {'\n'} 
+                    forma fácil
+                </Text>
 
-            <Text style={styles.title}>
-                Gerencie {'\n'}
-                suas plantas de {'\n'} 
-                forma fácil
-            </Text>
+                <Image 
+                    source={waterringImg} 
+                    style={styles.image}
+                    resizeMode="contain"
+                />
 
-            <Image 
-                source={waterringImg} 
-                style={styles.image}
-                resizeMode="contain"
-            />
+                <Text style={styles.subtitle}>
+                    Não esqueça mais de regar suas plantas. Nós cuidamos de lembrar você sempre que precisar.
+                </Text>
 
-            <Text style={styles.subtitle}>
-                Não esqueça mais de regar suas plantas. Nós cuidamos de lembrar você sempre que precisar.
-            </Text>
-
-            <TouchableOpacity style={styles.button} activeOpacity={0.7}>
-                {/* <Text style={styles.buttonText}> */}
-                    <Feather name="chevron-right" style={styles.buttonIcon}/>
-                {/* </Text> */}
-            </TouchableOpacity>
-
+                <TouchableOpacity style={styles.button} activeOpacity={0.7}>
+                        <Feather name="chevron-right" style={styles.buttonIcon}/>
+                </TouchableOpacity>
+            </View>
         </SafeAreaView>
     )
 }
@@ -46,22 +46,29 @@ export function Welcome(){
 const styles = StyleSheet.create({
     container: {
         flex: 1,
+
+    },
+    wrapper: {
+        flex: 1,
         alignItems: 'center',
-        justifyContent: 'space-around'
+        justifyContent: 'space-around',
+        paddingHorizontal: 20
     },
     title: {
-        fontSize: 32,
+        fontSize: 28,
         fontWeight: 'bold',
         textAlign: 'center',
         color: colors.heading,
-        marginTop: 38
+        marginTop: 38,
+        fontFamily: fonts.heading,
+        lineHeight: 34
     },
     subtitle: {
         textAlign: 'center',
         fontSize: 18,
         paddingHorizontal: 20,
-        // marginBottom: 38,
-        color: colors.heading
+        color: colors.heading,
+        fontFamily: fonts.text
     },
     image: {
         height: Dimensions.get('window').width * 0.7
